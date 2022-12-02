12f582b6374fd8a95264f6250f9210b7fd695011
rustamgadjiev <rustamgadjiev@list.ru>
2022-12-02 19:16:16 +0300

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,22 @@
-import { Item } from "./components/Item";
+import { useState } from "react";
 import "./App.css";
-import { useEffect } from "react";
+import Board from "./components/Board";
 
 function App() {
-  const arr = [
-    { isTree: false, id: 0 }, { isTree: false, id: 1 }, { isTree: false, id: 2 }, { isTree: false, id: 3 }, { isTree: false, id: 4 },
-    { isTree: false, id: 5 }, { isTree: true, id: 6 }, { isTree: false, id: 7 }, { isTree: false, id: 8 }, { isTree: false, id: 9 },
-    { isTree: false, id: 10 }, { isTree: false, id: 11 }, { isTree: false, id: 12 }, { isTree: false, id: 13 }, { isTree: false, id: 14 },
-    { isTree: true, id: 15 }, { isTree: true, id: 16 }, { isTree: false, id: 17 }, { isTree: false, id: 18 }, { isTree: false, id: 19 },
-    { isTree: false, id: 20 }, { isTree: false, id: 21 }, { isTree: false, id: 22 }, { isTree: false, id: 23 }, { isTree: true, id: 24 },
-  ];
+  const [level, setLevel] = useState(1);
 
-  const treesIds = [];
-
-  useEffect(() => {
-    arr.forEach(item => {
-      if (item.isTree) {
-        treesIds.push(item.id);
-      };
-    });
-  }, []);
+  const handleClick = (e) => {
+    setLevel(e.target.textContent);
+  }
 
   return (
     <div className="App">
-      {/* <div className="inner">
-        <div className="num">2</div>
-        <div className="num">0</div>
-        <div className="num">1</div>
-        <div className="num">1</div>
-        <div className="num">1</div>
-      </div> */}
-      <div className="inner">
-        {arr.map((item) => {
-          return <Item obj={item} arr={arr} />
-        })}
+      <div>
+        Уровни
+        <button onClick={handleClick}>1</button>
+        <button onClick={handleClick}>2</button>
       </div>
+      <Board level={level} />
     </div>
   );
 }
